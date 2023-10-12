637d46971c17bf13ac9a4eaa7e38b4fc7535b26c
Diego Moraes <diegofrost96@gmail>
2023-10-12 20:41:38 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/dlmoraes/gofinance-backend/api"
+	db "github.com/dlmoraes/gofinance-backend/db/sqlc"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env fifle")
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+
+	if err != nil {
+		log.Fatal("cannot start api: ", err)
+	}
+}
---
+++ util/random.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+func RandomString(number int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < number; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomEmail(number int) string {
+	return fmt.Sprintf("%s@email.com", RandomString(number))
+}
